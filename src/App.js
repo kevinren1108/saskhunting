e88a4ca9964eadeaf5664247f4c14312876e6c24
Kevin Ren <kevinren1108@gmail.com>
2022-03-30 22:33:32 -0600

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        买枪
+        <br/>
+        <a href={"https://www.rangeviewsports.ca/"}>华威枪店(找群主买有折扣)</a>
+        <br/>
+        <a href={"https://www.gotenda.com/"}>腾达枪店</a>
+        <br/>
+        <a href={"https://g4cgunstore.com/"}>G4C枪店</a>
+        <br/>
+        <a href={"https://www.cabelas.ca/"}>坎贝拉</a>
+        <br/>
+        <a href={"https://www.prairieguntraders.ca/"}>小镇枪店(里贾纳往南50km)</a>
+        <br/>
+        <a href={"https://tntgunworks.com/"}>Tnt枪店(里贾纳市内)</a>
+        <br/>
+        <a href={"https://bit.ly/3qSwSgM"}>私人之间枪支转让收据范本</a>
+        <br/>
+        靶场
+        <br/>
+        <a href={"https://rfgl.net/"}>北靶场</a>
+        <br/>
+        <a href={"https://reginawildlifefederation.com/"}>东靶场</a>
+        <br/>
+        <a href={"http://www.reginatrapandskeet.com/"}>飞碟靶场</a>
+        <br/>
+        买猎票
+        <br/>
+        <a href={"https://www.permis-permits.ec.gc.ca/en/PurchaseHuntingPermit"}>购买联邦鸟票</a>
+        <br/>
+        <a href={"https://www.saskatchewanhal.ca/licensing.page"}>购买萨省猎票</a>
+        <br/>
+        <a href={"http://environment.gov.sk.ca/adx/aspx/adxGetMedia.aspx?DocID=3365f7b7-c444-4170-be39-4ef629d041c4"}>萨省猎票购买及使用指南</a>
+        <br/>
+        狩猎指南
+        <br/>
+        <a href={"https://publications.saskatchewan.ca/api/v1/products/68827/formats/93283/download"}>2022年萨省狩猎指南</a>
+        <br/>
+        法律
+        <br/>
+        <a href={"https://pubsaskdev.blob.core.windows.net/pubsask-prod/2841/W13-1r1.pdf"}>野生动物管理法</a>
+        <br/>
+        <a href={"https://pubsaskdev.blob.core.windows.net/pubsask-prod/archived/37041/1930-CH-208.pdf"}>猎物法</a>
+        <br/>       
     </div>
   );
 }
