c5e3766cc04367995edac13906175d5d40ef89de
kevinren1108 <kevinren1108@gmail.com>
2023-09-04 20:21:00 -0700

    update footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./pages/home";
 import Toolbar from "./components/toolbar.js";
 import { Route, Routes} from "react-router-dom";
 import Account from "./pages/account";
+import Footer from "./components/footer";
 
 function App() {
 
@@ -12,10 +13,12 @@ function App() {
       {/* check if at the account page */}
       {/* { location.pathname === "/account" ? <></>: <Toolbar /> } */}
       <Toolbar />
+      
         <Routes>
             <Route path="/" exact element={<Home />}></Route>
             <Route path="/account" exact element={<Account />} ></Route>
         </Routes>   
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from '../styles/Home.module.css'
+
+
+function Footer() {
+  return (
+    <div className={styles.footer}>
+        Saskatchewan Hunting Information Site 2022. All Rights Reserved.
+    </div>
+  )
+}
+
+export default Footer
